c192df403e4ba424065fee649d95b0db512d769d
Gretiks <156473423+Gretiks@users.noreply.github.com>
2025-06-16 10:33:27 +0200

    Add files via upload

---
+++ VetManager/Model/Visit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetManager.Model
+{
+    internal class Visit
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public Animal Animal { get; set; }
+
+        public Visit(int id, DateTime date, string type, string description, Animal animal)
+        {
+            this.Id = id;
+            this.Date = date;
+            this.Type = type;
+            this.Description = description;
+            this.Animal = animal;
+        }
+
+    }
+}
